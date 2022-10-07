da7481c28329a1f998e9eb2d8bd7e670a42b9311
pankajthakurtech <115213567+pankajthakurtech@users.noreply.github.com>
2022-10-07 13:09:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -39,37 +39,19 @@ function showMovies(movies) {
           main.appendChild(movieEl);
 
      });
-
-    // main.innerHTML = ''    <img src="${IMG_PATH +poster_path}" alt ="${title}">
-
-    // movies.forEach((movie) => {
-    //     const { title, poster_path, vote_average, overview } = movie
-
-    //     const movieEl = document.createElement('div')
-    //     movieEl.classList.add('movie')
-
-    //     movieEl.innerHTML = `
-    //         <img src="${IMG_PATH + poster_path}" alt="${title}">
-    //         <div class="movie-info">
-    //       <h3>${title}</h3>
-         
-    //         </div>
-    //         <div class="overview">
-    //       <h3>Overview</h3>
-    //       ${overview}
-    //     </div>
-    //     `
-    //     main.appendChild(movieEl)
-    // })
 }
 
-// function getClassByRate(vote) {
-//     if(vote >= 8) {
-//         return 'green'
-//     } else if(vote >= 5) {
-//         return 'orange'
-//     } else {
-//         return 'red'
-//     }
-// }
 
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const serachTerm = search.value
+    if(serachTerm && serachTerm !==''){
+         getMovies(SEARCH_API + searchTerm)
+        search.value='';
+    }else{
+        window.location.reload();
+    }
+
+})
